b1193a977c142501b948a950dac503fe3c951fbd
bboti199 <b.boti199@yahoo.com>
2019-11-10 15:00:40 +0200

    finished

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -4,11 +4,19 @@ import io from 'socket.io-client';
 
 import './Chat.css';
 
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
+import TextContainer from '../TextContainer/TextContainer';
+
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('');
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -27,9 +35,45 @@ const Chat = ({ location }) => {
     };
   }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    socket.on('message', message => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users);
+    });
+
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    };
+  }, [messages]);
+
+  const sendMessage = e => {
+    e.preventDefault();
+
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  console.log(message, messages);
+
   return (
-    <div>
-      <p>Chat</p>
+    <div className='outerContainer'>
+      <div className='container'>
+        <InfoBar room={room} />
+
+        <Messages messages={messages} name={name} />
+
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
     </div>
   );
 };
